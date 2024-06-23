ea35b3b742e0b52030b45a491563ab1b9097fc16
cindellai <cindellai26@gmail.com>
2024-06-23 16:55:30 -0500

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,16 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :integer
+#  user_id    :integer
 #
 class Post < ApplicationRecord
   validates(:title, presence: true)
   validates(:body, presence: true)
   validates(:expires_on, presence: true)
+
+  def owner
+    matching_users = User.where({ :id => self.user_id })
+    the_user = matching_users.at(0)
+    return the_user
+  end
 end
